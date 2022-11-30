a85aecbc39a33256d5d29721fc6c38e1b9db4ffa
MayYoY <947109682@qq.com>
2022-11-30 14:27:04 +0800

    add metrics calculate

---
+++ evaluate/metric.py
@@ -0,0 +1,54 @@
+import torch
+import numpy as np
+
+from . import postprocess
+
+
+class Accumulate:
+    def __init__(self, n):
+        self.cnt = [0] * n
+        self.acc = [0] * n
+        self.avg = [0] * n
+
+    def update(self, val, n):
+        self.cnt += n
+        self.acc += val
+        self.avg = self.acc / self.cnt
+
+
+def cal_metric(preds: torch.Tensor, labels: torch.Tensor, config, post="fft", methods=None):
+    """
+    :param preds:
+    :param labels:
+    :param config:
+    :param post: 后处理计算 phys 的方法 fft or peak
+    :param methods: 评估指标
+    :return:
+    """
+    if methods is None:
+        methods = ["MAE", "RMSE", "MAPE", "R"]
+    if post == "fft":
+        pred_phys = postprocess.fft_physiology(preds.cpu().numpy(), target=config.target,
+                                               Fs=config.Fs, diff=config.diff,
+                                               detrend_flag=config.detrend_flag).reshape(-1)
+        label_phys = postprocess.fft_physiology(labels.cpu().numpy(), target=config.target,
+                                                Fs=config.Fs, diff=config.diff,
+                                                detrend_flag=config.detrend_flag).reshape(-1)
+    else:
+        pred_phys = postprocess.peak_physiology(preds.cpu().numpy(), target=config.target,
+                                                Fs=config.Fs, diff=config.diff,
+                                                detrend_flag=config.detrend_flag).reshape(-1)
+        label_phys = postprocess.peak_physiology(labels.cpu().numpy(), target=config.target,
+                                                 Fs=config.Fs, diff=config.diff,
+                                                 detrend_flag=config.detrend_flag).reshape(-1)
+    ret = [] * len(methods)
+    for m in methods:
+        if m == "MAE":
+            ret.append(np.abs(pred_phys - label_phys).mean())
+        elif m == "RMSE":
+            ret.append(np.sqrt((np.square(pred_phys - label_phys)).mean()))
+        elif m == "MAPE":
+            ret.append((np.abs((pred_phys - label_phys) / label_phys)).mean() * 100)
+        elif m == "R":
+            ret.append(np.corrcoef(pred_phys, label_phys)[0, 1])
+    return ret
