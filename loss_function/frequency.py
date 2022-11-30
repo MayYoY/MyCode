28deafcfaa6aab2158920f0a23dcc1c8cb21432f
MayYoY <947109682@qq.com>
2022-11-30 22:12:19 +0800

    modify device setting

--- loss_function/frequency.py
+++ loss_function/frequency.py
@@ -7,13 +7,12 @@ import torch.nn as nn
 
 # TODO: 短序列心率计算不准确
 class HRCELoss(nn.Module):
-    def __init__(self, T=300, delta=3, reduction="mean", use_snr=False, device="cuda"):
+    def __init__(self, T=300, delta=3, reduction="mean", use_snr=False):
         """
         :param T: 序列长度
         :param delta: 信号带宽, 带宽外的认为是噪声, 验证阶段的 delta 为 60 * 0.1
         :param reduction:
         :param use_snr:
-        :param device:
         """
         super(HRCELoss, self).__init__()
         self.T = T
@@ -21,18 +20,15 @@ class HRCELoss(nn.Module):
         self.low_bound = 40
         self.high_bound = 150
         # for DFT
-        self.bpm_range = torch.arange(self.low_bound, self.high_bound,
-                                      dtype=torch.float, device=device) / 60.
-        self.two_pi_n = Variable(2 * math.pi * torch.arange(0, self.T, dtype=torch.float,
-                                                            device=device))
+        self.bpm_range = torch.arange(self.low_bound, self.high_bound, dtype=torch.float) / 60.
+        self.two_pi_n = Variable(2 * math.pi * torch.arange(0, self.T, dtype=torch.float))
         self.hanning = Variable(torch.from_numpy(np.hanning(self.T)).type(torch.FloatTensor),
-                                requires_grad=True).view(1, -1).to(device)  # 1 x N
+                                requires_grad=True).view(1, -1)  # 1 x N
         # criterion
         self.reduction = reduction
         self.cross_entropy = nn.CrossEntropyLoss(reduction=reduction)
 
         self.use_snr = use_snr
-        self.device = device
 
     def forward(self, wave, labels, fps):
         """
@@ -42,6 +38,11 @@ class HRCELoss(nn.Module):
         :param fps: B,
         :return:
         """
+        # 多 GPU 训练下, 确保同一 device
+        self.bpm_range = self.bpm_range.to(wave.device)
+        self.two_pi_n = self.two_pi_n.to(wave.device)
+        self.hanning = self.hanning.to(wave.device)
+
         # DFT
         B = wave.shape[0]
         # DFT 中的 k = N x fk / fs, x N 与 (-j2 \pi n **k** / N) 抵消
@@ -66,7 +67,7 @@ class HRCELoss(nn.Module):
 
         if self.use_snr:
             # 归一化
-            norm_t = (torch.ones(B, device=self.device) / torch.sum(complex_absolute, dim=1))
+            norm_t = (torch.ones(B, device=wave.device) / torch.sum(complex_absolute, dim=1))
             norm_t = norm_t.view(-1, 1)  # B x 1
             complex_absolute = complex_absolute * norm_t  # B x range
             # CE loss
