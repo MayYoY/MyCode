d2e0d35e9c9cf22c598f9be48e8aeca0da7fbff5
MayYoY <947109682@qq.com>
2022-12-04 22:25:59 +0800

    add hrv calculation

--- evaluate/postprocess.py
+++ evaluate/postprocess.py
@@ -1,15 +1,35 @@
 import numpy as np
 import scipy
 import scipy.io
-from scipy.signal import butter, periodogram, find_peaks
+from scipy.signal import butter, periodogram, find_peaks, firwin, lombscargle
 from scipy.sparse import spdiags
+from scipy import interpolate
+
+
+def bandpass_hamming(Fs, taps_num=128, cutoff=None):
+    """
+    bandpass filtered (128-point Hamming window, 0.7–4 Hz)
+    :return:
+    """
+    if cutoff is None:
+        cutoff = [0.7, 4]
+    return firwin(numtaps=taps_num, cutoff=cutoff, window="hamming",
+                  fs=Fs, pass_zero="bandpass")
+
+
+def average_filter(wave, win_size=5):
+    """five-point moving average filter"""
+    """temp = np.zeros(len(wave) + win_size - 1)
+    temp[win_size // 2: win_size // 2 + len(wave)] = wave
+    return np.convolve(temp, np.ones(win_size) / win_size, mode="valid")"""
+    return np.convolve(wave, np.ones(win_size) / win_size, mode="same")
 
 
 def mag2db(mag):
     return 20. * np.log10(mag)
 
 
-def detrend(signal, lambda_value):
+def detrend(signal, lambda_value=100):
     """
     :param signal: T, or B x T
     :param lambda_value:
@@ -129,14 +149,47 @@ def peak_physiology(signal: np.ndarray, target="pulse", Fs=30, diff=True, detren
     return np.asarray(phys)
 
 
-def cal_hrv(signal: np.ndarray, target="pulse", Fs=30, diff=True, detrend_flag=True):
+def cal_hrv(wave, Fs, bandpass=True, interpolation=True):
     """
-    计算 HRV: LF, HF, LF / HF, 
-    :param signal:
-    :param target:
+     detrend -> normalize -> filters -> interpolate -> calculate PSD -> get hrv
+    :param wave:
     :param Fs:
-    :param diff:
-    :param detrend_flag:
+    :param bandpass: 
+    :param interpolation: 发现插值的效果更好, 更接近 heartpy
     :return:
     """
-    pass
+    temp = wave.copy()
+    ret = [0.] * 4  # LF, HF, LF / HF, RR
+    # detrend
+    temp = detrend(temp, lambda_value=10)
+    # normalize
+    temp = (temp - temp.mean()) / temp.std()
+    # five-point moving average filter and bandpass filtered
+    temp = average_filter(temp)
+    if bandpass:
+        b = bandpass_hamming(Fs)
+        temp = scipy.signal.filtfilt(b, 1, temp)
+    if interpolation:
+        # interpolated with a cubic spline function at a sampling frequency of 256 Hz.
+        fun = interpolate.CubicSpline(range(len(temp)), temp)
+        x = np.linspace(0, len(temp) - 1, num=round(256 / Fs * len(temp)))
+        y = fun(x)
+    else:
+        y = temp.copy()
+    #  HRV was performed by PSD estimation using the Lomb periodogram
+    x = np.linspace(0, (len(y) - 1) / 256, num=len(y))  # 采样时间, 假设为均匀采样
+    freq = np.arange(1, 501) / 1000  # 需要计算的频率点
+    psd = lombscargle(x, y, freq, normalize=True)
+    # LF, HF:the area under the PSD curve corresponding
+    # to 0.04–0.15 and 0.15–0.4 Hz, respectively
+    mask1 = np.argwhere((0.15 >= freq) & (freq >= 0.04))  # LF
+    mask2 = np.argwhere((0.4 >= freq) & (freq >= 0.15))  # HF
+    ret[0] = psd[mask1].sum()
+    ret[1] = psd[mask2].sum()
+    ret[2] = ret[0] / ret[1]
+    # calculate RR from the center frequency of the HF peak f_{HFpeak} in the HRV PSD
+    hf_freq = freq[mask2]
+    hf_psd = psd[mask2]
+    ret[3] = 60 * hf_freq[hf_psd.argmax()][0]
+    # print(f"LF: {ret[0]}; HF: {ret[1]}; LF/HF: {ret[2]:.3f}; RR: {ret[3]:.3f}")
+    return ret
